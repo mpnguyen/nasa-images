be15e7048d5e222eb0bd22b2bbc1b286b171bffd
Phu Nguyen <mpnguyen@Phus-MacBook-Pro.local>
2020-05-09 11:34:16 +0700

    Initial project

---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,13 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ *
+ */
+
+import React from 'react';
+import './home-page.scss';
+
+export default function HomePage() {
+  return <h1 className="title">Nasa Images</h1>;
+}
